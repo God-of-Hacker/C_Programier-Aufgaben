5720c5d424f362645fde6254c08fe1e46a953e62
God-of-Hacker <92095573+God-of-Hacker@users.noreply.github.com>
2021-11-03 15:45:36 +0100

    Sensor Zeughausstrasse

--- C EVA Zusatz/main.c
+++ C EVA Zusatz/main.c
@@ -38,6 +38,21 @@
 int main(void)
 {
     //Variablen
+    uint8_t schalter = 0;
+    uint8_t S5 = 0; 
+    uint8_t S6 = 0; 
+    uint8_t S7 = 0; 
+    uint8_t S8 = 0; 
+    uint8_t S9 = 0;
+    uint8_t S10 = 0;
+    uint8_t S11 = 0;
+    uint16_t H0 = 0;
+    uint16_t H1 = 0;
+    uint16_t H2 = 0;
+    uint16_t H3 = 0;
+    uint16_t H4 = 0;
+    uint16_t H5 = 0;
+    uint16_t H6 = 0;
     
     //Initialisieren
     initBoard(1);
@@ -46,10 +61,32 @@ int main(void)
     while(1)
     {
         //Eingabe------------------------------------------------------------------
+        schalter = switchReadAll();
+        S5 = schalter & (1<<0);
+        S6 = schalter & (1<<1);
+        S7 = schalter & (1<<3);
+        S8 = schalter & (1<<4);
+        S9 = schalter & (1<<5);
+        S10 = schalter & (1<<6);
+        S11 = schalter & (1<<7);
+        
+        
         
         //Verarbeitung-------------------------------------------------------------
         
+        if (S5)
+        {
+            H0 = H0 | (0x01);
+        } 
+        else
+        {
+            H0 = H0 & ~(0x01);
+        }
+        
+        
         //Ausgabe------------------------------------------------------------------
+        
+        ledWriteAll(H0);
         
     }
 }
