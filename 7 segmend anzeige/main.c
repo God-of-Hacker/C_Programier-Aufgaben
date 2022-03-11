f74fbdd4e39931b75cf262812f737a23da45a079
God-of-Hacker <92095573+God-of-Hacker@users.noreply.github.com>
2022-03-11 10:49:01 +0100

    Gray-Code mit Fehlerkontrolle

--- 7 segmend anzeige/main.c
+++ 7 segmend anzeige/main.c
@@ -63,14 +63,16 @@
 #define  SCHALTER_EINER  0b00001111
 #define  SCHALTER_ZEHNER 0b11110000
 
+#pragma GCC optimize 0
 
 //const uint8_t DECORDER[] = {ZAHL_7SEG_0, ZAHL_7SEG_1, ZAHL_7SEG_2, ZAHL_7SEG_3, ZAHL_7SEG_4, ZAHL_7SEG_5,
                           //  ZAHL_7SEG_6, ZAHL_7SEG_7, ZAHL_7SEG_8, ZAHL_7SEG_9, ZAHL_7SEG_10,
                            // ZAHL_7SEG_11, ZAHL_7SEG_12, ZAHL_7SEG_13, ZAHL_7SEG_14, ZAHL_7SEG_15};
                             
-                            const uint8_t GRAY_TO_DEZ[] = {ZAHL_7SEG_0,ZAHL_7SEG_1,ZAHL_7SEG_3,ZAHL_7SEG_2,ZAHL_7SEG_7,ZAHL_7SEG_6,
+                            const uint8_t GRAY_TO_SEG[] = {ZAHL_7SEG_0,ZAHL_7SEG_1,ZAHL_7SEG_3,ZAHL_7SEG_2,ZAHL_7SEG_7,ZAHL_7SEG_6,
                                                    ZAHL_7SEG_4,ZAHL_7SEG_5,ZAHL_7SEG_15,ZAHL_7SEG_14,ZAHL_7SEG_12,ZAHL_7SEG_13,
                                                    ZAHL_7SEG_8,ZAHL_7SEG_9,ZAHL_7SEG_11,ZAHL_7SEG_10}; //Gray-Code
+                             const uint8_t GRAY_TO_DEZ[] = {0,1,3,2,7,6,4,5,15,14,12,13,8,9,11,10};
 
 //Hauptprogramm
 int main(void)
@@ -80,7 +82,9 @@ int main(void)
     uint8_t schalter2 = 0;
     uint8_t zahl = 0;
     uint8_t inGray =0;
-    uint8_t inDez =0;
+    uint8_t inDezAlt =0;
+    uint8_t inDezNeu =0;
+    uint8_t inSeg = 0;
     //Initialisieren
     initBoard(1);
     DDRD = 0xff; //Alle Bits von PortD als Ausgang konfugurieren
@@ -88,8 +92,20 @@ int main(void)
     while(1)
     {
       inGray = switchReadAll() & SCHALTER_EINER;  
-      inDez = GRAY_TO_DEZ[inGray];
-      PORTD = inDez;    
+      inSeg = GRAY_TO_SEG[inGray];
+      
+      inDezNeu = GRAY_TO_DEZ[inGray];     
+      if (((inDezNeu == inDezAlt +1) || (inDezNeu == inDezAlt -1)|| (inDezNeu == inDezAlt))   )
+      {
+          PORTD = inSeg;
+          inDezAlt = inDezNeu;
+      } 
+      else
+      {
+          PORTD = SEGMENT_DP;
+      }
+      
+         
     }
 }
 
