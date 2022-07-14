bebf9bab19ea2b38af304c50a3ceffc85295d529
God-of-Hacker <92095573+God-of-Hacker@users.noreply.github.com>
2022-07-14 13:21:34 +0200

    a

--- PORT_Register/main.c
+++ PORT_Register/main.c
@@ -3,7 +3,7 @@
 * MMMMMMMMMMMM   SSSSSSSSSSSS   WW   WW   WW   MECHATRONIK
 * MM   MM   MM   SS             WW   WW   WW   SCHULE
 * MM   MM   MM   SSSSSSSSSSSS   WW   WW   WW   WINTERTHUR
-* MM   MM   MM             SS   WW   WW   WW   
+* MM   MM   MM             SS   WW   WW   WW
 * MM   MM   MM   SSSSSSSSSSSS   WWWWWWWWWWWW   www.msw.ch
 *
 *
@@ -33,29 +33,64 @@
 
 //uC-Board-Treiber hinzufügen
 #include "ucBoardDriver.h"
+#define  LED_0_1                (0b11<<0) //0b00000011
+#define  LED_2_3                (0b11<<2) //0b00001100
+#define  LED_4_5                (0b11<<4) //0b00110000
+#define  LED_6_7                (0b11<<6) //0b11000000
+
+
+//Gute Namen für Maske
+#define PULL_UO_SWITCHES
+#define DDR_LED_B
+#define DDR_SWITCHES
+#define OUT_LED_A
+#define OUT_LED_B
+#define IN_SWITCHES
+
 
 
 //Hauptprogramm
 int main(void)
 {
-    //Variablen
+    //Variablen                                                             C Bibel seite 31
     
     //Initialisieren
     initBoard(0);
-    //1) Eingang mit Pillup
-    DDRC = 0;
-    PORTC = 0;
     
-    //2) Ausgang alles Low
-    DDRA = 0xff;
-    PORTA = 0;
-    _delay_ms(3000);
+    DDRA=0xff;
+    PORTA=0;
+    while(1);
+    
+    
+    //1) Ausgang alles LED Low :                    für Ausgang muss DDRA auf 1 sein und für LED Low muss PORT auf 0 sein
+    DDRA = DDRA | LED_0_1;
+    PORTA = PORTA & (~LED_0_1);
+    
+    //2) Ausgang alles LED High :                   für Ausgang muss DDRA auf 1 sein und für LED High muss PORT auf 1 sein
+    DDRA = DDRA | LED_2_3;
+    PORTA = PORTA | LED_2_3;
+    
+    //3) Eingang mit Pullup :                       für Eingang muss DDRA auf 0 sein und mit Pullup muss PORT auf 1 sein
+    DDRA = DDRA & (~LED_4_5);
+    PORTA = PORTA | LED_4_5;
+    
+    //4) Eingang ohne Pullup :                      für Eingang muss DDRA auf 0 sein und ohne Pullup muss PORT auf 0 sein
+    DDRA = DDRA & (~LED_6_7);
+    PORTA = PORTA & (~LED_6_7);
+    
+    //5) Schalter Eingang mit Pullup
+    DDRC = 0;
+    PORTC = 0xff;
+    
+    //6) PORTB ist für die Anzeige an LED's und es muss auf Ausgang definiert sien also alles (1) ein
+    DDRB = 0xff;
     
     //Unendlichschlaufe
     while(1)
     {
-        //3) C einlesen, an A ausgeben
-        PORTA = PINC;
+        //7) für LED's zeigt PORT
+        PORTB = PINC;
+        
     }
 }
 
